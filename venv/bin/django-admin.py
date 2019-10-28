9085dc8da54950d0ed0b9b6a80774125dec3577b
UniCEUB <eduardo@eduardosan.com>
2019-10-28 04:58:13 -0400

    Remove completamente diretório

--- venv/bin/django-admin.py
+++
@@ -1,4 +0,0 @@
-#!/home/uniceub/eduardo/web/alunos/gabriel-projeto/venv/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==3.0b1','django-admin.py'
-__requires__ = 'Django==3.0b1'
-__import__('pkg_resources').run_script('Django==3.0b1', 'django-admin.py')
