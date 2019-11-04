a830c3ae8cb96687f6130066fba5bbb4d1a6774a
OtottoNinja <37624940+otottoninja@users.noreply.github.com>
2019-11-04 07:58:44 -0300

    perfil.html home=' ' base.html com perfil

--- home/url.py
+++ home/url.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('contato/', views.contato, name='contato'),
+    path('perfil/', views.perfil, name='perfil'),
 ]
